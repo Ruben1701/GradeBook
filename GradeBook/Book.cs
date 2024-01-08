0cb3bb06b946f4a9fb89b5f55a317eb35442ca6d
Ruben Hornung <ruben@cre8ion.com>
2024-01-08 15:54:26 +0100

    added show statistics

--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -8,6 +8,39 @@ namespace GradeBook
 {
     class Book
     {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                highGrade = Math.Max(grade, highGrade);
+                lowGrade = Math.Min(grade, lowGrade);
+
+                result += grade;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"For the book: {name}");
+            Console.WriteLine($"The highest grade is: {highGrade}");
+            Console.WriteLine($"The lowest grade is: {lowGrade}");
+            Console.WriteLine($"The average grade is: {result}");
+        }
+
+        private List<double> grades;
+        private string name;
 
     }
 }
--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,30 +1,18 @@
 ﻿using System;
 
 namespace GradeBook
-{ 
+{
     class Program
     {
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(56.1);
+            book.AddGrade(52.1);
+            book.AddGrade(90.1);
 
-            var grades = new List<double>();
-            grades.Add(56.1);
-            grades.Add(52.1);
-            grades.Add(90.1);
-
-            var result = 0.0;
-            foreach (var grade in grades)
-            {
-                result += grade;
-            }
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is: {result}");
-
-            if (args.Length > 0) {
-                Console.WriteLine($"Hello {args[0]}!");
-            }
-            else { Console.WriteLine("Hello!"); }
+            book.ShowStatistics();
         }
     }
 }
