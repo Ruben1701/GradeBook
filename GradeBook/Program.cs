b8459b59693342ceb637f74a1c296924cd9ccc0e
Ruben Hornung <ruben@cre8ion.com>
2024-01-08 13:42:32 +0100

    added book class

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
 namespace GradeBook
-{
-
+{ 
     class Program
     {
         static void Main(string[] args)
         {
+            var book = new Book();
+
+            var grades = new List<double>();
+            grades.Add(56.1);
+            grades.Add(52.1);
+            grades.Add(90.1);
+
+            var result = 0.0;
+            foreach (var grade in grades)
+            {
+                result += grade;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is: {result}");
+
             if (args.Length > 0) {
                 Console.WriteLine($"Hello {args[0]}!");
             }
